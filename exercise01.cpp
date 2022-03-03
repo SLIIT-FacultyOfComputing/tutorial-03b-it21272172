661de0e7bfdd186725eaeb91dc24c87dce5eb43f
it21272172 <99705224+it21272172@users.noreply.github.com>
2022-03-03 20:07:52 +0530

    Update exercise01.cpp

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
@@ -7,6 +8,6 @@ int main() {
  for (int r = 0; r < 5; r++) {
      cout <<  r+1 
           <<  names[r]
-          << marks[r] << endl;
+          << setiosflags(ios::fixed) << setprecision(2) << marks[r] << endl;
  }
 }
